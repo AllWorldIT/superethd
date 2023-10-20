d7ef2366cf142a1edba0cb7caa561ddfc6002c70
Nigel Kukard <nkukard@lbsd.net>
2023-10-20 17:04:22 +0000

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## License
+
+This project is licensed under AGPLv3 or a commercial license at your option. For the full AGPLv3 license text, see the `LICENSE`
+file in the repository. For commercial licensing options, please contact Conarx, Ltd at sales@conarx.tech.
--- tests/t_200-checksums.cpp
+++ tests/t_200-checksums.cpp
@@ -1,3 +1,21 @@
+/*
+ * Checksum testing.
+ * Copyright (C) 2023, AllWorldIT.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "libtests/framework.hpp"
 
 
@@ -6,7 +24,7 @@ TEST_CASE("Checking checksums generated are correct", "[checksums]") {
 
 
 	// TODO: convert to c++
-	
+
 	char *test_buffer = create_sequence_data(0xffff);
 
 	uint32_t partial_checksum = compute_checksum_partial((uint8_t *)test_buffer, buffer_size, 0);
